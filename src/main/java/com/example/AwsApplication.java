713bd1bfbfbf758eb21c2da068594f5f1a6b24bc
Muhammad Tapash <muhammadtapash@Muhammads-MacBook-Air.local>
2023-11-30 12:19:19 -0500

    first commit

---
+++ src/main/java/com/example/AwsApplication.java
@@ -0,0 +1,26 @@
+package com.example;
+
+
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AwsApplication {
+	
+	@GetMapping("/hi")
+	public String message() {
+		
+		return "welcome to WES";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsApplication.class, args);
+	}
+
+}
